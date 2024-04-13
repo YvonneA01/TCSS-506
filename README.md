51fb512a4fe9bdab24eef40362b13cc387dd9521
Yvonne <missyve2001@gmail.com>
2024-04-13 01:07:49 -0500

    bash script to install sshfs and mount

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# TCSS-506
+Script Details
+The script checks if sshfs is installed and installs it if necessary.
+
+It creates a mount point directory (/mnt/aws) if it doesn't already exist.
+
+It mounts mount the home directory on my aws instance to this mount point if it is not mounted using sshfs.
+
+The script uses SSH key-based authentication (assuming SSH keys are properly configured) to authenticate without requiring password input.
+
+Notes
+Ensure that SSH access is allowed and SSH key-based authentication is properly configured on the remote server.
+
+Adjust the mount point directory (MOUNT_POINT) and sshfs options (-o allow_other) as needed based on the need case.
+
+
+
---
+++ install_sshfs_mount.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+ # Check if sshfs is installed; if not, install it
+if ! command -v sshfs &> /dev/null; then
+    echo "sshfs is not installed. Installing..."
+    sudo apt update
+    sudo apt install -y sshfs
+fi
+
+# Verify if sshfs installation was successful
+if ! command -v sshfs &> /dev/null; then
+    echo "Error: sshfs installation failed. Please install sshfs manually and try again."
+    exit 1
+fi
+
+# Define mount point directory
+MOUNT_POINT="/mnt/aws"
+
+# Create mount point directory if it doesn't exist
+if [ ! -d "$MOUNT_POINT" ]; then
+    echo "Creating mount point directory: $MOUNT_POINT"
+     mkdir -p "$MOUNT_POINT"
+fi
+
+# Check if the mount point is already mounted
+if mountpoint -q "$MOUNT_POINT"; then
+    echo "The mount point $MOUNT_POINT is already mounted."
+    exit 0
+fi
+
+
+# Mount home directory of the ubuntu user to the mount point using sshfs
+echo "Mounting home directory of ubuntu to $MOUNT_POINT..."
+ sshfs -o IdentityFile=~/Downloads/sshkey.pem  -o workaround=rename -o allow_other ubuntu@ec2-3-144-108-65.us-east-2.compute.amazonaws.com:/home/ubuntu "$MOUNT_POINT"
+
+# Check if mount was successful
+if [ $? -eq 0 ]; then
+    echo "Mount successful!"
+else
+    echo "Mount failed. Please check the SSH connection and try again."
+fi
